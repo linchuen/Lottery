3375cee9422e67d52df5b04cce49c248d3ffb11a
Aiden <aiden.lin@ambernet.com.tw>
2023-12-29 17:22:45 +0800

    新增generator-config

--- src/main/java/com/cooba/LotteryApplication.java
+++ src/main/java/com/cooba/LotteryApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.cooba.TradeSimulator.mapper")
+@MapperScan("com.cooba.mapper")
 @SpringBootApplication
 public class LotteryApplication {
 
