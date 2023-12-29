7f178f0e6af476983637be0787a9490060e872d7
Aiden <aiden.lin@ambernet.com.tw>
2023-12-29 17:09:38 +0800

    新增設定

--- src/main/java/com/cooba/LotteryApplication.java
+++ src/main/java/com/cooba/LotteryApplication.java
@@ -1,8 +1,10 @@
 package com.cooba;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.cooba.TradeSimulator.mapper")
 @SpringBootApplication
 public class LotteryApplication {
 
