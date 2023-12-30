fbe8be2601811a8dfe2a7b20c304cd1a0da4bd25
Aiden <x29587629@gmail.com>
2023-12-30 22:18:51 +0800

    完成SqlLotteryNumberRepository

--- src/main/java/com/cooba/repository/lotteryNumber/SqlLotteryNumberRepository.java
+++ src/main/java/com/cooba/repository/lotteryNumber/SqlLotteryNumberRepository.java
@@ -1,5 +1,7 @@
 package com.cooba.repository.lotteryNumber;
 
+import com.cooba.mapper.LotteryNumberMapper;
+import com.cooba.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +10,11 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class SqlLotteryNumberRepository implements LotteryNumberRepository {
+    private final LotteryNumberMapper lotteryNumberMapper;
 
     @Override
     public boolean insertNumber(int lotteryId, long round, List<Integer> winningNumbers) {
-        return true;
+        int result = lotteryNumberMapper.insertNumbers(lotteryId, round, JsonUtil.toJsonString(winningNumbers));
+        return result == 1;
     }
 }
