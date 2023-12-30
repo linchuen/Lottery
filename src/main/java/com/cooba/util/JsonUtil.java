164690144f1faa53b4d27ff3dd303006b5a77ab0
Aiden <x29587629@gmail.com>
2023-12-30 12:49:14 +0800

    調整OrderEntity

---
+++ src/main/java/com/cooba/util/JsonUtil.java
@@ -0,0 +1,36 @@
+package com.cooba.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public class JsonUtil {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> String toJsonString(T value) {
+        try {
+            return objectMapper.writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T parseObject(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> List<T> parseList(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, new TypeReference<>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/resources/sql/order_entity.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `order_entity` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `player_id` bigint(20) DEFAULT NULL,
+  `wallet_id` int(11) DEFAULT NULL,
+  `asset_id` int(11) DEFAULT NULL,
+  `round` bigint(20) DEFAULT NULL,
+  `game_code` varchar(20) DEFAULT NULL,
+  `number_string` varchar(20) DEFAULT NULL,
+  `bet_amount` decimal(16,4) DEFAULT NULL,
+  `odds` decimal(16,4) DEFAULT NULL,
+  `bet_prize` decimal(16,4) DEFAULT NULL,
+  `fee` decimal(16,4) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `game_status` int(11) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `updated_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
